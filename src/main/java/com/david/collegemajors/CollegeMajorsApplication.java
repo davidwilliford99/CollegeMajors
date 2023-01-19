1b474ee49718a30a5a98d0afa7f3360630a4666c
davidwilliford99 <d9899w@gmail.com>
2023-01-19 17:33:09 -0500

    Configured new laptop to work on this project

--- src/main/java/com/david/collegemajors/CollegeMajorsApplication.java
+++ src/main/java/com/david/collegemajors/CollegeMajorsApplication.java
@@ -6,19 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Welcome to the MajorStatistics API!
  *
- *
- * -----------------------------------------------------------------
- *
- * For the default username ande password for the dev environment:
- *
- * username = user
- * pw = {whatever the console gives you}
- * To logout of API, go localhost:8080/logout
- *
- *
- *------------------------------------------------------------------
- *
- *
  */
 
 @SpringBootApplication
